f75b9fd04587677df7e0d6403517ee3b1cc20e38
guilhermesurica.contato <guilhermesurica.contato@outlook.com>
2023-03-16 20:13:32 -0300

    first commit, traning react legacy code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    name: "Guilherme Surica",
+    counter: 0
+  }
+
+  handleAClick = (e) => {
+    e.preventDefault();
+    const { counter } = this.state
+    this.setState({ counter: counter + 1 })
+  }
+
+  handlePClick = () => {
+    this.setState({ name: "Gui" })
+  }
+
+  render() {
+
+    const { name, counter } = this.state
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            { name }
+          </p>
+          <p>
+            { counter }
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Este é o link!
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
