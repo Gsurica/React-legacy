223ca147d732a1b4bdd20bd1d0eda1b59f1653cd
guilhermesurica.contato <guilhermesurica.contato@outlook.com>
2023-03-22 08:00:33 -0300

    react - starts unit tests

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
+import { Button } from "."
+
+describe('<Button />', () => {
+  it('should render the button with text "Load more"', () => {
+    render(<Button text="Load more" />);
+    expect.assertions(1);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should call function when the button get clicked', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" onClick={fn} />)
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    userEvent.click(button)
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be disabled when disabled attibute is true', () => {
+    render(<Button text='Load more' disable={true} />)
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeDisabled();
+  }); 
+
+  it('should be enabled when disable is false', () => {
+    render(<Button text="Load more" disable={false} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeEnabled();
+  }) 
+
+});
