942f23b274eaec829b770ca17d963b7275bff924
guilhermesurica.contato <guilhermesurica.contato@outlook.com>
2023-03-23 20:52:03 -0300

    creating the paragraph test

--- src/components/PostCard/PostCard.spec.jsx
+++ src/components/PostCard/PostCard.spec.jsx
@@ -7,8 +7,9 @@ const props = postCardMock
 describe('<PostCard />', () => {
   it('should render PostCard correctly', () => {
     render(<PostCard {...props} />)
-    
+
     expect(screen.getByRole('img', { name: /something/i })).toHaveAttribute('src', 'https//site.com.br')
     expect(screen.getByRole('heading', { name: /something/i })).toBeInTheDocument()
+    expect(screen.getByText(/body 1/i)).toBeInTheDocument()
   })
 })
