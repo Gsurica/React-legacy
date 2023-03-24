31f7acc32062339523f23923ed8911f3fb475f10
guilhermesurica.contato <guilhermesurica.contato@outlook.com>
2023-03-23 21:30:56 -0300

    creating the posts tests

--- src/components/Posts/Post.spec.jsx
+++ src/components/Posts/Post.spec.jsx
@@ -1,3 +1,41 @@
+const { render, screen } = require("@testing-library/react")
+import { Posts } from './index' 
+
+const props = {
+  posts: [
+    {
+      id: 1,
+      title: 'title 1',
+      body: 'body 1', 
+      cover: 'testing image 1'
+    }, 
+    {
+      id: 2,
+      title: 'title 2',
+      body: 'body 2', 
+      cover: 'testing image 2'
+    }, 
+    {
+      id: 3,
+      title: 'title 3',
+      body: 'body 3', 
+      cover: 'testing image 3'
+    }, 
+  ]
+}
+
 describe('<Posts />', () => {
-  
+  it('should render posts', () => {
+    render(<Posts { ...props } />)
+
+    expect(screen.getAllByRole('heading', { name: /title/i })).toHaveLength(3)
+    expect(screen.getAllByRole('img', { name: /title/i })).toHaveLength(3)
+    expect(screen.getAllByText(/body/i)).toHaveLength(3)
+    expect(screen.getByRole('img', { name: /title 3/i })).toHaveAttribute('src', 'testing image 3')
+  }) 
+
+  it('should match the posts snapshot', () => {
+    const { container } = render(<Posts {...props} />) 
+    expect(container.firstChild).toMatchSnapshot()
+  })
 })
