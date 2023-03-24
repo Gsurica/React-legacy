a654471d6746fbae2dbcd6cf5fc35522f2fda022
guilhermesurica.contato <guilhermesurica.contato@outlook.com>
2023-03-23 21:58:50 -0300

    creating the input tests

---
+++ src/components/TextInput/TextInput.spec.jsx
@@ -0,0 +1,25 @@
+import { screen, render } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
+import { TextINput } from ".";
+
+describe('<TextInput />', () => {
+  it('should value be the default value passed', () => {
+    const fn = jest.fn()
+    render(<TextINput handleChange={fn} searchValue={'test'} />)
+
+    const input = screen.getByPlaceholderText(/Type your search!@/i)
+    expect(input.value).toBe('test')
+  })
+
+  it('should call handle change func on each key pressed', () => {
+    const fn = jest.fn()
+    render(<TextINput handleChange={fn} />)
+
+    const input = screen.getByPlaceholderText(/type your search!@/i)
+    const value = 'the value'
+
+    userEvent.type(input, value)
+    expect(input.value).toBe(value)
+    expect(fn).toHaveBeenCalledTimes(value.length)
+  })  
+})
--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -3,7 +3,7 @@ import './styles.css'
 export const TextINput = ({ searchValue, handleChange }) => {
   return (
     <input 
-      class="text-input"
+      className="text-input"
       value={searchValue}
       type="search" 
       onChange={handleChange}
